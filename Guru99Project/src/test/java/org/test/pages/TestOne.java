96480a78c32db459266ec69d7fab5aa5d98c4c02
SasikiranKakara <55825519+sasikirankakara001@users.noreply.github.com>
2023-12-17 13:15:37 +0530

    commit

--- Guru99Project/src/test/java/org/test/pages/TestOne.java
+++ Guru99Project/src/test/java/org/test/pages/TestOne.java
@@ -1,10 +1,17 @@
 package org.test.pages;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
+
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -13,12 +20,24 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestOne {
 	public static WebDriver driver;
+	public static Properties props;
 	
 	@BeforeTest
-	public void start() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("https://www.demo.guru99.com/V4/");
+	public void start() throws IOException {
+		props = new Properties();
+		File fs = new File(System.getProperty("user.dir")+"\\src\\test\\java\\utils\\config.properties");
+		FileInputStream fis = new FileInputStream(fs);
+		props.load(fis);
+		if (props.getProperty("browser").equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(props.getProperty("browser").equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else {
+			driver = new EdgeDriver();
+		}
+		driver.get(props.getProperty("url"));
 	}
 	@AfterTest
 	public void end() {
@@ -27,8 +46,10 @@ public class TestOne {
 
 	@Test
 	public void ExampleOne() throws Exception {
-		driver.findElement(By.name("uid")).sendKeys("mngr542415");
-		driver.findElement(By.name("password")).sendKeys("123456@12");
+		driver.findElement(By.name("uid")).sendKeys(props.getProperty("name"));
+		driver.findElement(By.name("password")).sendKeys(props.getProperty("password"));
 		driver.findElement(By.name("btnLogin")).click();
 	}
+	
+	
 }
